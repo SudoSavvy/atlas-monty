b8b8b3c61b22f42d6f232d86ffdcecd90cd4baba
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 16:49:43 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- main.c
+++ main.c
@@ -8,17 +8,17 @@ int main(int argc, char **argv)
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
+        return (EXIT_FAILURE);
     }
 
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
+        return (EXIT_FAILURE);
     }
 
-    /* Initialize and run your Monty interpreter here */
+    /* Use stack and line_number in your Monty interpreter here */
 
     fclose(file);
     return (EXIT_SUCCESS);
