8b7f26f5a36cf9f2f8db6e451783eac5bbe2be11
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 17:32:04 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- execute_opcodes.c
+++ execute_opcodes.c
@@ -1,57 +1,58 @@
 #include "monty.h"
-#include <string.h>  // For strtok and strcmp
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+/**
+ * execute_opcodes - Executes the opcodes from the file
+ * @file: The file pointer to the bytecode file
+ * @stack: Pointer to the stack
+ */
 void execute_opcodes(FILE *file, stack_t **stack)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
+    char line[1024];
+    char *opcode, *arg;
     unsigned int line_number = 1;
-    char *opcode;
-    char *arg;
 
-    while ((read = getline(&line, &len, file)) != -1)
+    while (fgets(line, sizeof(line), file))
     {
-        // Remove trailing newline character if present
-        if (line[read - 1] == '\n')
-            line[read - 1] = '\0';
-
-        // Skip empty lines
-        if (line[0] == '\0')
+        opcode = strtok(line, " \t\n");
+        if (opcode == NULL || opcode[0] == '#')
         {
             line_number++;
             continue;
         }
+        arg = strtok(NULL, " \t\n");
 
-        // Tokenize the line
-        opcode = strtok(line, " \t");
-        arg = strtok(NULL, " \t");
-
-        // Handle opcode
-        if (opcode)
+        if (strcmp(opcode, "push") == 0)
         {
-            if (strcmp(opcode, "push") == 0)
-                push(stack, line_number, arg);
-            else if (strcmp(opcode, "pint") == 0)
-                pint(stack, line_number);
-            else if (strcmp(opcode, "pall") == 0)
-                pall(stack);
-            else if (strcmp(opcode, "pop") == 0)
-                pop(stack, line_number);
-            else if (strcmp(opcode, "swap") == 0)
-                swap(stack, line_number);
-            else if (strcmp(opcode, "add") == 0)
-                add(stack, line_number);
-            else
-            {
-                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-                free(line);
-                exit(EXIT_FAILURE);
-            }
+            push(stack, line_number, arg);
         }
-
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(stack);
+        }
+        else if (strcmp(opcode, "pop") == 0)
+        {
+            pop(stack, line_number);
+        }
+        else if (strcmp(opcode, "swap") == 0)
+        {
+            swap(stack, line_number);
+        }
+        else if (strcmp(opcode, "add") == 0)
+        {
+            add(stack, line_number);
+        }
+        else if (strcmp(opcode, "nop") == 0)
+        {
+            nop(stack, line_number);
+        }
+        else
+        {
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
         line_number++;
     }
-
-    free(line);
 }
--- nop.c
+++ nop.c
@@ -1,14 +1,14 @@
 #include "monty.h"
+#include <stdio.h>
 
 /**
- * nop - A no-operation function that does nothing
+ * nop - No operation
  * @stack: Pointer to the stack
  * @line_number: Line number in the bytecode file
- *
- * Return: None
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;       // Unused parameter
-    (void)line_number; // Unused parameter
+    (void)stack;
+    (void)line_number;
+    /* Does nothing */
 }
