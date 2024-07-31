ee7ee3718adc79f69f536de279bedbd414583552
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 15:12:57 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- push.c
+++ push.c
@@ -1,12 +1,17 @@
 #include "monty.h"
-#include <ctype.h>
+#include <ctype.h> // Include this header for isdigit
 
+/**
+ * push - Pushes an element to the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number of the current operation.
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
     char *arg = strtok(NULL, " \n");
     int n;
 
-    if (arg == NULL || !isdigit(*arg))
+    if (arg == NULL || !is_number(arg))
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
@@ -28,4 +33,25 @@ void push(stack_t **stack, unsigned int line_number)
         (*stack)->prev = new_node;
 
     *stack = new_node;
+}
+
+/**
+ * is_number - Checks if a string is a valid integer.
+ * @str: The string to check.
+ *
+ * Return: 1 if the string is a valid integer, 0 otherwise.
+ */
+int is_number(const char *str)
+{
+    if (*str == '-' || *str == '+')
+        str++;
+    if (!*str)
+        return 0;
+    while (*str)
+    {
+        if (!isdigit(*str))
+            return 0;
+        str++;
+    }
+    return 1;
 }
