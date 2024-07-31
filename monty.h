24f55bb89cd19a4861960b72cc773582a908d608
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 17:11:30 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- monty.h
+++ monty.h
@@ -21,5 +21,6 @@ void push(stack_t **stack, unsigned int line_number, char *arg);
 void pint(stack_t **stack, unsigned int line_number);
 void execute_opcodes(FILE *file, stack_t **stack);
 void free_stack(stack_t *stack);
+void pall(stack_t **stack);
 
 #endif /* MONTY_H */
