63d651150341a53dbfb8f86d24082c4b90592c6a
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 15:31:14 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- main.c
+++ main.c
@@ -2,69 +2,69 @@
 
 void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number)
 {
-    instruction_t instructions[] = {
-        {"push", push},
-        {"pall", pall},
-        {NULL, NULL}
-    };
+instruction_t instructions[] = {
+{"push", push},
+{"pall", pall},
+{NULL, NULL}
+};
 
-    int i = 0;
-    while (instructions[i].opcode != NULL)
-    {
-        if (strcmp(opcode, instructions[i].opcode) == 0)
-        {
-            instructions[i].f(stack, line_number);
-            return;
-        }
-        i++;
-    }
+int i = 0;
+while (instructions[i].opcode != NULL)
+{
+if (strcmp(opcode, instructions[i].opcode) == 0)
+{
+instructions[i].f(stack, line_number);
+return;
+}
+i++;
+}
 
-    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-    exit(EXIT_FAILURE);
+fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+exit(EXIT_FAILURE);
 }
 
 void free_stack(stack_t *stack)
 {
-    stack_t *temp;
-    while (stack != NULL)
-    {
-        temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+stack_t *temp;
+while (stack != NULL)
+{
+temp = stack;
+stack = stack->next;
+free(temp);
+}
 }
 
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    stack_t *stack = NULL;
-    unsigned int line_number = 0;
+FILE *file;
+char *line = NULL;
+size_t len = 0;
+stack_t *stack = NULL;
+unsigned int line_number = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
 
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+file = fopen(argv[1], "r");
+if (file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
 
-    while (getline(&line, &len, file) != -1)
-    {
-        line_number++;
-        char *opcode = strtok(line, " \n");
-        if (opcode != NULL && opcode[0] != '#')
-            execute_instruction(opcode, &stack, line_number);
-    }
+while (getline(&line, &len, file) != -1)
+{
+line_number++;
+char *opcode = strtok(line, " \n");
+if (opcode != NULL && opcode[0] != '#')
+execute_instruction(opcode, &stack, line_number);
+}
 
-    free(line);
-    fclose(file);
-    free_stack(stack);
-    return 0;
+free(line);
+fclose(file);
+free_stack(stack);
+return 0;
 }
--- push.c
+++ push.c
@@ -9,50 +9,50 @@ int is_number(const char *str);
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    char *arg = strtok(NULL, " \n");
-    int n;
+char *arg = strtok(NULL, " \n");
+int n;
 
-    if (arg == NULL || !is_number(arg))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+if (arg == NULL || !is_number(arg))
+{
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
+exit(EXIT_FAILURE);
+}
 
-    n = atoi(arg);
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+n = atoi(arg);
+stack_t *new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *stack;
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
+if (*stack != NULL)
+(*stack)->prev = new_node;
 
-    *stack = new_node;
+*stack = new_node;
 }
 
 /**
- * is_number - Checks if a string is a valid integer.
- * @str: The string to check.
- *
- * Return: 1 if the string is a valid integer, 0 otherwise.
- */
+* is_number - Checks if a string is a valid integer.
+* @str: The string to check.
+*
+* Return: 1 if the string is a valid integer, 0 otherwise.
+*/
 int is_number(const char *str)
 {
-    if (*str == '-' || *str == '+')
-        str++;
-    if (!*str)
-        return 0;
-    while (*str)
-    {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
-    return 1;
+if (*str == '-' || *str == '+')
+str++;
+if (!*str)
+return 0;
+while (*str)
+{
+if (!isdigit(*str))
+return 0;
+str++;
+}
+return 1;
 }
