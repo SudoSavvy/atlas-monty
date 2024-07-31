b17355751e0e9b5e445cb87855bfa90a873271b1
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 15:59:45 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- monty.h
+++ monty.h
@@ -5,15 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
-* struct stack_s - doubly linked list representation of a stack (or queue)
-* @n: integer
-* @prev: points to the previous element of the stack (or queue)
-* @next: points to the next element of the stack (or queue)
-*
-* Description: doubly linked list node structure
-* for stack, queues, LIFO, FIFO
-*/
+extern char *global_variable; /* If you have a global variable */
+
+/* Data structures */
 typedef struct stack_s
 {
 int n;
@@ -21,21 +15,18 @@ struct stack_s *prev;
 struct stack_s *next;
 } stack_t;
 
-/**
-* struct instruction_s - opcode and its function
-* @opcode: the opcode
-* @f: function to handle the opcode
-*
-* Description: opcode and its function
-* for stack, queues, LIFO, FIFO
-*/
 typedef struct instruction_s
 {
 char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* Function prototypes */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+int is_number(const char *str);
+void free_stack(stack_t *stack);
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number);
 
-#endif
+#endif /* MONTY_H */
