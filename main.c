c0d00b43794fd99ed3ff556cbdceebde46a8cbf5
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 17:01:06 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- main.c
+++ main.c
@@ -1,51 +1,28 @@
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/**
- * execute_opcodes - Executes the opcodes from the Monty bytecode file.
- * @file: Pointer to the file containing Monty bytecode.
- * @stack: Pointer to the stack.
- */
-void execute_opcodes(FILE *file, stack_t **stack)
+int main(int argc, char *argv[])
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    unsigned int line_number = 1;
+    FILE *file;
+    stack_t *stack = NULL;
 
-    while ((read = getline(&line, &len, file)) != -1)
+    if (argc != 2)
     {
-        char *opcode = strtok(line, " \t\n");
-        if (opcode == NULL || *opcode == '#') /* Skip empty lines or comments */
-        {
-            line_number++;
-            continue;
-        }
-
-        if (strcmp(opcode, "push") == 0)
-        {
-            char *arg = strtok(NULL, " \t\n");
-            if (arg == NULL || !is_number(arg))
-            {
-                fprintf(stderr, "L%u: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            push(stack, atoi(arg));
-        }
-        else if (strcmp(opcode, "pint") == 0)
-        {
-            pint(stack, line_number);
-        }
-        else
-        {
-            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
-
-        line_number++;
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
     }
 
-    free(line);
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    execute_opcodes(file, &stack);
+    fclose(file);
+
+    free_stack(stack);
+    return (EXIT_SUCCESS);
 }
