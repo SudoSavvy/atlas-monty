5c533c8b39b7bfc0fd111be07a2910a582567ce0
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 15:08:07 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# atlas-monty
+# atlas-monty readme page <3 
+
+I know how to make one i'd like to reserve big amazing ones for bigger projects like the one that just passed
---
+++ push.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+void push(stack_t **stack, unsigned int line_number)
+{
+    char *arg = strtok(NULL, " \n");
+    int n;
+
+    if (arg == NULL || !isdigit(*arg))
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    n = atoi(arg);
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+
+    *stack = new_node;
+}
