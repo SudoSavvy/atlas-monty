61028273f177be41c4fc78f747c0a7c12a011157
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 16:51:01 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+void execute_opcodes(FILE *file, stack_t **stack);
 
 int main(int argc, char **argv)
 {
+    stack_t *stack = NULL;
+
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
@@ -15,8 +22,52 @@ int main(int argc, char **argv)
         return (EXIT_FAILURE);
     }
 
-    /* Initialize and run your Monty interpreter here */
+    execute_opcodes(file, &stack);
 
     fclose(file);
     return (EXIT_SUCCESS);
+}
+
+void execute_opcodes(FILE *file, stack_t **stack)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    unsigned int line_number = 1;
+
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        /* Parse and execute each line */
+        /* For simplicity, assuming only 'push' and 'pint' instructions */
+        char *opcode = strtok(line, " \t\n");
+        if (opcode == NULL || *opcode == '#')  /* Skip empty lines or comments */
+        {
+            line_number++;
+            continue;
+        }
+
+        if (strcmp(opcode, "push") == 0)
+        {
+            char *arg = strtok(NULL, " \t\n");
+            if (arg == NULL || !is_number(arg))
+            {
+                fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                exit(EXIT_FAILURE);
+            }
+            push(stack, atoi(arg));
+        }
+        else if (strcmp(opcode, "pint") == 0)
+        {
+            pint(stack, line_number);
+        }
+        else
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+
+        line_number++;
+    }
+
+    free(line);
 }
--- pint.c
+++ pint.c
@@ -1,17 +1,18 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*pint - Prints the value at the top of the stack.
-*@stack: Double pointer to the head of the stack.
-*@line_number: Line number of the current operation.
-*/
+ * pint - Prints the value at the top of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Current line number in the bytecode file.
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
-if (*stack == NULL)
-{
-fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-exit(EXIT_FAILURE);
-}
-
-printf("%d\n", (*stack)->n);
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*stack)->n);
 }
