381fdd8c075fb85517d2453645fd5ca3c90bcbe8
SudoSavvy <donovanjheape@gmail.com>
2024-07-31 17:11:45 -0500

    Stacks, Queues, LIFO, FIFO programming in c

--- pall.c
+++ pall.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack)
 {
-(void)line_number;
-stack_t *current = *stack;
+    stack_t *current = *stack;
 
-while (current != NULL)
-{
-printf("%d\n", current->n);
-current = current->next;
-}
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
